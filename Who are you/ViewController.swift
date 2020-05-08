2643d9f28bfe19a3d94134bf9d6001a4c08030dd
Maxim <55534590+Glomis@users.noreply.github.com>
2020-05-08 21:59:55 +0300

    Initial Commit

---
+++ Who are you/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Who are you
+//
+//  Created by Tatyana on 08.05.2020.
+//  Copyright © 2020 Max&Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
